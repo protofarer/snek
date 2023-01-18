f8039d02145ef1a5b9a0f5dd4e84e3e0bcc98612
Kenny Baron <kennybaron@fastmail.com>
2023-01-18 11:26:12 -0500

    build bundle includes app version; eslint include node env

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,35 @@
 import { 
   defineConfig, 
-  // loadEnv 
+  loadEnv 
 } from 'vite'
 
-export default defineConfig({
-  // root: 'src',
-  // envDir: '../',
-  // publicDir: '../public',
-  server: {
-    open: true,
-    host: '0.0.0.0',
-    port: 3001,
-    // usePolling: true,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        index: 'index.js',
-      },
-      output: {
-        dir: 'dist'
-      },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log(`app vers`, env.VITE_APP_VERSION)
+  
+  return {
+    // root: 'src',
+    // envDir: '../',
+    // publicDir: '../public',
+    server: {
+      open: true,
+      host: '0.0.0.0',
+      port: 3001,
+      // usePolling: true,
     },
-    // emptyOutDir: true,
-    // sourcemap: true,
-  },
-  main: new URL('./lab/lab.html', import.meta.url),
+    build: {
+      rollupOptions: {
+        input: {
+          index: 'index.js',
+        },
+        output: {
+          dir: 'dist',
+          entryFileNames: `index-v${env.VITE_APP_VERSION}.js`
+        },
+      },
+      // emptyOutDir: true,
+      // sourcemap: true,
+    },
+    main: new URL('./lab/lab.html', import.meta.url),
+  }
 })
