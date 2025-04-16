dff33675cd63d901b8851136bcb856c4221858cd
Kenny Baron <kennybaron@fastmail.com>
2025-04-16 19:32:44 -0400

    improve start screen instructions; obfuscate eslint file

--- game/states/StartState.js
+++ game/states/StartState.js
@@ -1,52 +1,52 @@
-import BaseState from './BaseState'
-import ModalButton from '../../components-canvas/ModalButton'
-import Constants from '../Constants.js'
+import BaseState from "./BaseState";
+import ModalButton from "../../components-canvas/ModalButton";
+import Constants from "../Constants.js";
 
 export class StartState extends BaseState {
   modes = [
-    'Survival', 
-    'Test', 
+    "Survival",
+    "Test",
     // 'Normal'
-  ]
-  mode = 0
-  stateName = 'start'
+  ];
+  mode = 0;
+  stateName = "start";
 
   constructor(game) {
-    super()
-    this.game = game
-    this.game.panel.panelContainer.style.setProperty('visibility', 'hidden')
-    this.game.sounds.melody1.currentTime = 0
-    this.game.sounds.melody1.play()
+    super();
+    this.game = game;
+    this.game.panel.panelContainer.style.setProperty("visibility", "hidden");
+    this.game.sounds.melody1.currentTime = 0;
+    this.game.sounds.melody1.play();
 
     // TODO adapt to whether user is on mobile or desktop
     this.handleKeyDown = (e) => {
       switch (e.key) {
-        case 'ArrowDown':
-          this.mode = (++this.mode) % this.modes.length
-          break
-        case 'ArrowUp':
-          this.mode = (
-              --this.mode < 0 ? this.modes.length - 1 : this.mode
-            ) % this.modes.length
-          break
-        case 'Enter':
-          if (this.modes[this.mode] === 'Survival') {
-            this.game.stateMachine.change('playSurvival')
-          } 
+        case "ArrowDown":
+          this.mode = ++this.mode % this.modes.length;
+          break;
+        case "ArrowUp":
+          this.mode =
+            (--this.mode < 0 ? this.modes.length - 1 : this.mode) %
+            this.modes.length;
+          break;
+        case "Enter":
+          if (this.modes[this.mode] === "Survival") {
+            this.game.stateMachine.change("playSurvival");
+          }
           // else if (this.modes[this.mode] === 'Normal') {
           //   this.game.stateMachine.change('playNormal', {
           //     level: 1,
           //   })
           // }
-          break
+          break;
       }
-    }
-    document.addEventListener('keydown', this.handleKeyDown)
+    };
+    document.addEventListener("keydown", this.handleKeyDown);
 
     // const normalButtData = {
-    //   origin: { 
-    //     x: this.game.canvas.width * 0.35, 
-    //     y: this.game.canvas.height * 0.35, 
+    //   origin: {
+    //     x: this.game.canvas.width * 0.35,
+    //     y: this.game.canvas.height * 0.35,
     //   },
     //   base: { w: 125, },
     //   label: 'Normal Mode',
@@ -63,31 +63,30 @@ export class StartState extends BaseState {
     // this.normalButt.show()
 
     const survivalButtData = {
-      origin: { 
-        x: this.game.canvas.width * 0.35, 
-        y: this.game.canvas.height * 0.4444
+      origin: {
+        x: this.game.canvas.width * 0.35,
+        y: this.game.canvas.height * 0.4444,
       },
-      base: { w: 125, },
-      label: 'Survival Mode',
-    }
+      base: { w: 125 },
+      label: "Survival Mode",
+    };
     this.survivalButt = new ModalButton(
       null,
       this.game.ctx,
       survivalButtData,
-      () => this.game.stateMachine.change('playSurvival', {
-      }),
-      { once: true }
-    )
-    this.survivalButt.show()
+      () => this.game.stateMachine.change("playSurvival", {}),
+      { once: true },
+    );
+    this.survivalButt.show();
 
     const testButtData = {
-      origin: { 
-        x: this.game.canvas.width * 0.35, 
-        y: this.game.canvas.height * 0.55, 
+      origin: {
+        x: this.game.canvas.width * 0.35,
+        y: this.game.canvas.height * 0.55,
       },
-      base: { w: 125, },
-      label: 'Test: kade data',
-    }
+      base: { w: 125 },
+      label: "Test: kade data",
+    };
 
     // this.testButt = new ModalButton(
     //   null,
@@ -100,11 +99,11 @@ export class StartState extends BaseState {
     // )
     // this.testButt.show()
 
-    this.game.touchAreaContainer.style.display = 'none'
+    this.game.touchAreaContainer.style.display = "none";
     const params = new Proxy(new URLSearchParams(window.location.search), {
-      get: (searchParams, prop) => searchParams.get(prop)
-    })
-    this.appMode = params.mode
+      get: (searchParams, prop) => searchParams.get(prop),
+    });
+    this.appMode = params.mode;
   }
 
   update() {
@@ -114,57 +113,81 @@ export class StartState extends BaseState {
   }
 
   render() {
-    this.game.ctx.beginPath()
-    this.game.ctx.fillStyle = 'hsla(135, 70%, 35%)'
+    this.game.ctx.beginPath();
+    this.game.ctx.fillStyle = "hsla(135, 70%, 35%)";
     this.game.ctx.fillRect(
-      0, 0, 
-      this.game.canvas.width, this.game.canvas.height
-    )
+      0,
+      0,
+      this.game.canvas.width,
+      this.game.canvas.height,
+    );
 
     // show left and right touch control areas
-    this.game.ctx.strokeStyle = 'hsl(240,50%,50%)'
-    const linewidth = 4
-    this.game.ctx.lineWidth = linewidth
-    this.game.ctx.strokeRect(linewidth/2, linewidth/2, (this.game.canvas.width / 2) - (linewidth/ 2), this.game.canvas.height - (linewidth))
-    this.game.ctx.strokeRect((this.game.canvas.width / 2) + (linewidth/2), linewidth/2, (this.game.canvas.width/2) - (linewidth), this.game.canvas.height - (linewidth))
+    this.game.ctx.strokeStyle = "hsl(240,50%,50%)";
+    const linewidth = 4;
+    this.game.ctx.lineWidth = linewidth;
+    this.game.ctx.strokeRect(
+      linewidth / 2 + 5,
+      360,
+      this.game.canvas.width / 2 - linewidth / 2 - 5,
+      150,
+    );
+    this.game.ctx.strokeRect(
+      this.game.canvas.width / 2 + linewidth / 2 + 5,
+      360,
+      this.game.canvas.width / 2 - linewidth - 5,
+      150,
+    );
 
-    this.game.ctx.font = '16px Arial'
-    this.game.ctx.fillStyle = 'darkred'
+    this.game.ctx.font = "16px Arial";
+    this.game.ctx.fillStyle = "darkred";
 
-    this.game.ctx.fillText('HOLD LEFT AREA', 10, 125)
-    this.game.ctx.fillText('TO TURN LEFT', 10, 140)
-    this.game.ctx.fillText('HOLD RIGHT AREA', 215, 125)
-    this.game.ctx.fillText('TO TURN RIGHT', 215, 140)
-
+    this.game.ctx.fillText("HOLD LEFT AREA", 30, 425);
+    this.game.ctx.fillText("TO TURN LEFT", 30, 440);
+    this.game.ctx.fillText("HOLD RIGHT AREA", 220, 425);
+    this.game.ctx.fillText("TO TURN RIGHT", 220, 440);
 
     // this.normalButt.render()
-    this.survivalButt.render()
+    this.survivalButt.render();
     // this.testButt.render()
 
-    const yInstructions = 420
-    const xInstructions = 30
-    this.game.ctx.fillStyle = 'darkred'
-    this.game.ctx.fillText('portrait orientation only',xInstructions,yInstructions )
-    this.game.ctx.fillText('A: turn left',xInstructions , yInstructions + 25)
-    this.game.ctx.fillText('D: turn right',xInstructions, yInstructions + 50)
-    this.game.ctx.fillText('<space>: action',xInstructions, yInstructions + 75)
-
-    this.game.ctx.font = '10px Mono'
-    const xFooter = 10
-    const yFooter = 540
-    this.game.ctx.fillText(`app mode: ${this.appMode}`, xFooter, yFooter)
-    this.game.ctx.fillText(`prototype v${import.meta.env.VITE_APP_VERSION}: ${Constants.VERSION_DESCRIPTION}`, xFooter, yFooter + 15)
+    const yInstructions = 50;
+    const xInstructions = 30;
+    this.game.ctx.fillStyle = "darkred";
     this.game.ctx.fillText(
-      `created by Kenny Baron @ kennybaron.net 2022-${new Date().getFullYear()}`, 
-      xFooter, yFooter + 30
-    )
+      "portrait orientation only",
+      xInstructions,
+      yInstructions,
+    );
+    this.game.ctx.fillText("A: turn left", xInstructions, yInstructions + 25);
+    this.game.ctx.fillText("D: turn right", xInstructions, yInstructions + 50);
+    // this.game.ctx.fillText(
+    //   "<space>: action",
+    //   xInstructions,
+    //   yInstructions + 75,
+    // );
 
-    this.game.world.render()
+    this.game.ctx.font = "10px Mono";
+    const xFooter = 10;
+    const yFooter = 540;
+    // this.game.ctx.fillText(`app mode: ${this.appMode}`, xFooter, yFooter);
+    // this.game.ctx.fillText(
+    //   `prototype v${import.meta.env.VITE_APP_VERSION}: ${Constants.VERSION_DESCRIPTION}`,
+    //   xFooter,
+    //   yFooter + 15,
+    // );
+    this.game.ctx.fillText(
+      `created by Kenny Baron @ www.kennybaron.net 2022`,
+      xFooter,
+      yFooter + 30,
+    );
+
+    this.game.world.render();
   }
 
   exit() {
-    this.game.sounds.melody1.pause()
-    document.removeEventListener('keydown', this.handleKeyDown)
+    this.game.sounds.melody1.pause();
+    document.removeEventListener("keydown", this.handleKeyDown);
 
     // recycle(this.snek)
     // recycle(this.apple1)
@@ -172,12 +195,12 @@ export class StartState extends BaseState {
 
     // ! TODO removeEventListener in class Button not working as intended
     // this.normalButt.removeClickListener()
-    this.survivalButt.removeClickListener()
+    this.survivalButt.removeClickListener();
     // ! workaround
     // this.normalButt.path = new Path2D()
-    this.survivalButt.path = new Path2D()
-    this.game.panel.panelContainer.style.setProperty('visibility', 'visible')
-    this.game.touchAreaContainer.style.display = 'flex'
+    this.survivalButt.path = new Path2D();
+    this.game.panel.panelContainer.style.setProperty("visibility", "visible");
+    this.game.touchAreaContainer.style.display = "flex";
     // this.game.rightTouchArea.style.visibility = 'hidden'
     // this.game.leftTouchArea.style.visibility = 'hidden'
   }
